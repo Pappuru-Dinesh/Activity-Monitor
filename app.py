b5c63862babaf6bce9cada079d4955c236313663
Pappuru-Dinesh <36697509+Pappuru-Dinesh@users.noreply.github.com>
2024-06-03 17:22:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,36 +2,39 @@ import gradio as gr
 from tensorflow.keras.models import load_model
 import numpy as np
 import pandas as pd
-from io import StringIO
 
 # Load the trained model
-model = load_model('activity_cnn_lstm_model.h5')
+model = load_model('activity_cnn_lstm_model_regular.h5')
 
 # Define the sensor columns
 sensor_columns = ['alx', 'aly', 'alz', 'glx', 'gly', 'glz', 'arx', 'ary', 'arz', 'grx', 'gry', 'grz']
 
-def predict_activity(csv_input):
-    # Read the CSV input into a DataFrame
-    df = pd.read_csv(StringIO(csv_input), header=None, names=sensor_columns)
+# Activity names
+activity_names = {0: 'Standing still', 1: 'Lying down', 2: 'Walking', 3: 'Climbing stairs',
+                  4: 'Cycling', 5: 'Jogging', 6: 'Running'}
+
+def predict_activity(file_path):
+    # Read the uploaded CSV file into a DataFrame
+    df = pd.read_csv(file_path)
     
-    # Convert the DataFrame to the correct data type
-    df = df.astype(float)
-    
+    # Restrincting to only 100 rows
+    df = df[:100]
+
     # Ensure the input shape matches the model's expected input shape
     if df.shape[0] != 100 or df.shape[1] != len(sensor_columns):
-        return "Error: The input CSV must have 100 rows and the correct number of columns."
+        return f"Error: The input CSV must have 100 rows and the correct number of columns. {df.shape}"
     
     inputs_array = df.values.reshape(1, 100, len(sensor_columns))
     
     # Make predictions
     prediction = model.predict(inputs_array)
     activity = np.argmax(prediction, axis=1)[0]
-    return f'Predicted Activity: {activity}'
+    return f'Predicted Activity: {activity_names[activity]}'
 
 # Define the Gradio interface
 interface = gr.Interface(
     fn=predict_activity,
-    inputs=gr.Textbox(lines=20, placeholder="Paste your CSV content here..."),
+    inputs=gr.File(type="filepath", label="Upload your CSV file"),
     outputs=gr.Textbox(),
     title='Activity Prediction',
     description='Predict the activity based on sensor data using CNN-LSTM model. Ensure your CSV has 100 rows and the correct number of columns.'
